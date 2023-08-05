5a97d52bedf795a6f6833735de2e670d98a37e3d
aaadarsh2825 <100376291+aaadarsh2825@users.noreply.github.com>
2023-08-05 16:41:51 +0530

    Add files via upload

--- AtmInterface/ATM.java
+++ AtmInterface/ATM.java
@@ -1,1 +1,63 @@
+package myatmproject;
+import java.util.Scanner;
+
+public class ATM {
+    private BankAccount userAccount;
+    private Scanner scanner;
+
+    public ATM(BankAccount userAccount) {
+        this.userAccount = userAccount;
+        this.scanner = new Scanner(System.in);
+    }
+
+    public void start() {
+        System.out.println("Welcome to the ATM!");
+        while (true) {
+            System.out.println("\nSelect an option:");
+            System.out.println("1. Check Balance");
+            System.out.println("2. Withdraw");
+            System.out.println("3. Deposit");
+            System.out.println("4. Exit");
+
+            int choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    checkBalance();
+                    break;
+                case 2:
+                    withdraw();
+                    break;
+                case 3:
+                    deposit();
+                    break;
+                case 4:
+                    System.out.println("Thank you for using the ATM!");
+                    return;
+                default:
+                    System.out.println("Invalid choice. Please select a valid option.");
+            }
+        }
+    }
+
+    private void checkBalance() {
+        System.out.println("Your account balance: " + userAccount.getBalance());
+    }
+
+    private void withdraw() {
+        System.out.print("Enter the amount to withdraw: ");
+        double amount = scanner.nextDouble();
+        if (userAccount.withdraw(amount)) {
+            System.out.println("Withdrawal successful.");
+        } else {
+            System.out.println("Insufficient balance or invalid amount.");
+        }
+    }
+
+    private void deposit() {
+        System.out.print("Enter the amount to deposit: ");
+        double amount = scanner.nextDouble();
+        userAccount.deposit(amount);
+        System.out.println("Deposit successful.");
+    }
+}
 
